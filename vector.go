7666717471d615bb22c863a79f450991bfcfa0d9
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-21 23:42:24 +0200

    add vector methods

--- vector.go
+++ vector.go
@@ -20,3 +20,29 @@ func (v Vector) Normalize() Vector {
   l := v.Length()
   return Vector{v.X / l, v.Y / l, v.Z / l}
 }
+
+func (v Vector) Add(o Vector) Vector {
+	return Vector{v.X + o.X, v.Y + o.Y, v.Z + o.Z}
+}
+
+
+func (v Vector) Sub(o Vector) Vector {
+	return Vector{v.X - o.X, v.Y - o.Y, v.Z - o.Z}
+}
+
+
+func (v Vector) AddScalar(t float64) Vector {
+    return Vector{v.X + t, v.Y + t, v.Z + t}
+}
+
+func (v Vector) SubtractScalar(t float64) Vector {
+    return Vector{v.X - t, v.Y - t, v.Z - t}
+}
+
+func (v Vector) MultiplyScalar(t float64) Vector {
+    return Vector{v.X * t, v.Y * t, v.Z * t}
+}
+
+func (v Vector) DivideScalar(t float64) Vector {
+    return Vector{v.X / t, v.Y / t, v.Z / t}
+} 
