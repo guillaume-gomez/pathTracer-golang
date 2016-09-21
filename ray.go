e234126b15f4771abb0c6e0ecf9552d20424711b
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-21 23:45:52 +0200

    get current point function

--- ray.go
+++ ray.go
@@ -4,3 +4,11 @@ package main
 type Ray struct {
   Origin, Direction Vector
 }
+
+
+func (r Ray) Point(t float64) Vector {
+	// p(t)  =  A + B * t
+	b := r.Direction.MultiplyScalar(t)
+	a := r.Origin
+	return a.Add(b)
+}
