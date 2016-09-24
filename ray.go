b0c588e6a81fbdb9b62a5900cb51584bcd31e5cf
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-24 19:43:27 +0200

    add new function to check whether ray hit a sphere"

--- ray.go
+++ ray.go
@@ -7,8 +7,20 @@ type Ray struct {
 
 
 func (r Ray) Point(t float64) Vector {
-	// p(t)  =  A + B * t
-	b := r.Direction.MultiplyScalar(t)
-	a := r.Origin
-	return a.Add(b)
+  // p(t)  =  A + B * t
+  b := r.Direction.MultiplyScalar(t)
+  a := r.Origin
+  return a.Add(b)
 }
+
+func(r Ray) HitSphere(s sphere) bool {
+  // a*a + 2ab + c
+  oc := r.Origin.Sub(s.center)
+  a := r.Direction.Dot(r.Direction)
+  b := 2 * oc.Dot(r.Direction)
+  c := oc.Dot(oc) - s.Radius * s.Radius
+  discriminant := b*b - 4*a*c
+
+  return discriminant > 0
+}
+
