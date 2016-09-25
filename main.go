d607b9bea296e177cd4dfdace5077e60282e0db3
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-25 19:56:24 +0200

    move code

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
   "fmt"
   "os"
-  "math"
-  "math/rand"
   p_ "./primatives"
+  utils "./util"
 )
 
 const (
@@ -32,51 +31,6 @@ func check(e error, s string) {
   }
 }
 
-func colorize(r *p_.Ray, sphere p_.Sphere) p_.Vector {
-  hit, record := sphere.Hit(r, 0.0, math.MaxFloat64)
-
-  if hit {
-    return record.Normal.AddScalar(1.0).MultiplyScalar(0.5)
-  }
-
-  // make unit vector so y is between -1.0 and 1.0
-  unitDirection := r.Direction.Normalize()
-
-  return gradient(&unitDirection)
-}
-
-func gradient(v *p_.Vector) p_.Vector {
-  // scale t to be between 0.0 and 1.0
-  t := 0.5 * (v.Y + 1.0)
-
-  // linear blend: blended_value = (1 - t) * white + t * blue
-  return white.MultiplyScalar(1.0 - t).Add(blue.MultiplyScalar(t))
-}
-
-func gradientSphere(x int, nx int, y int, ny int,f *os.File) error {
-  rgb := p_.Vector{}
-  // sample rays for anti-aliasing
-    for s := 0; s < 100; s++ {
-      u := (float64(x) + rand.Float64()) / float64(nx)
-      v := (float64(y) + rand.Float64()) / float64(ny)
-
-      r := camera.RayAt(u, v)
-      color := colorize(&r, sphere)
-      rgb = rgb.Add(color)
-    }
-
-  // average
-  rgb = rgb.DivideScalar(float64(ns))
-
-  // get intensity of colors
-  ir := int(color * rgb.X)
-  ig := int(color * rgb.Y)
-  ib := int(color * rgb.Z)
-
-  _, err := fmt.Fprintf(f, "%d %d %d\n", ir, ig, ib)
-  return err
-}
-
 func main() {
   f, err := os.Create("out.ppm")
 
@@ -92,7 +46,7 @@ func main() {
   // from top left to bottom right
   for j := ny - 1; j >= 0; j-- {
     for i := 0; i < nx; i++ {
-      err = gradientSphere(i, nx, j, ny, f)
+      err = utils.GradientSphere(i, nx, j, ny, f)
       check(err, "Error writing to file: %v\n")
     }
   }
