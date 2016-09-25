79ab7ec1ff569aca8054ba439487367ddca2c3a4
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-25 16:57:31 +0200

    add tool method used in Sphere::Hit function

--- hitRecord.go
+++ hitRecord.go
@@ -4,3 +4,12 @@ type HitRecord struct {
   T         float64
   P, Normal Vector
 }
+
+func buildHitRecord(t float64, ray Ray, sphere Sphere) {
+  return HitRecord{
+    T: t,
+    P: ray.Point(t),
+    Normal: (ray.Point(t).Subtract(sphere.Center)).DivideScalar(sphere.Radius)
+  }
+}
+
