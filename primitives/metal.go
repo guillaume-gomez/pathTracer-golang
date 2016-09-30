ee9e949fdca88d31d309e802185a7dd36054e084
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-10-01 00:45:20 +0200

    add new material

---
+++ primitives/metal.go
@@ -0,0 +1,19 @@
+package primitives
+
+type Metal struct {
+  C    Vector
+  Fuzz float64
+}
+
+func (m Metal) Bounce(input Ray, hit HitRecord) (bool, Ray) {
+  direction := reflect(input.Direction, hit.Normal)
+  fuzzed := VectorInUnitSphere().MultiplyScalar(m.Fuzz)
+  bouncedRay := Ray{hit.Point, direction.Add(fuzzed)}
+  bounced := direction.Dot(hit.Normal) > 0
+  return bounced, bouncedRay
+}
+
+
+func (m Metal) Color() Vector {
+  return m.C
+}
