ace0d7944d9501b3b9165fd6e0bee1e5d26f6b91
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-09-30 20:30:16 +0200

    remove carriage returns

--- primatives/world.go
+++ primatives/world.go
@@ -8,10 +8,8 @@ func (w *World) Hit(r Ray, tMin float64, tMax float64) (bool, HitRecord) {
   hitAnything := false
   closest := tMax
   record := HitRecord{}
-
   for _, element := range w.Elements {
     hit, tempRecord := element.Hit(r, tMin, closest)
-
     if hit {
       hitAnything = true
       closest = tempRecord.T
