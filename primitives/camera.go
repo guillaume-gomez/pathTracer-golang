64209cee4c748e90015f61413bbabc556d60c616
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-10-15 15:42:27 +0200

    update camera add focus and blur

--- primitives/camera.go
+++ primitives/camera.go
@@ -5,31 +5,43 @@ import (
 )
 
 type Camera struct {
-  lowerLeft, horizontal, vertical, origin Vector
+  lowerLeft, horizontal, vertical, origin, u, v, w Vector
+  lensRadius                                       float64
+
 }
 
-func NewCamera(vfov, aspect float64) Camera {
-  return NewCameraWithPosition(Vector{0, 0, 0}, Vector{0, 0, -1}, Vector{0, 1, 0}, vfov, aspect)
+func NewCameraCentered(vfov, aspect, aperture float64) Camera {
+  lookFrom := Vector{0, 0, 0}
+  lookAt := Vector{0, 0, -1}
+  focusDist := lookFrom.Sub(lookAt).Length()
+  return NewCamera(lookFrom, lookAt , Vector{0, 1, 0}, vfov, aspect, aperture, focusDist)
 }
 
 
-func NewCameraWithPosition(lookFrom, lookAt, vUp Vector, vfov, aspect float64) Camera {
-
-  theta := vfov * math.Pi / 180
-  halfHeight := math.Tan(theta / 2)
-  halfWidth := aspect * halfHeight
-
+func NewCamera(lookFrom, lookAt, vUp Vector, vFov, aspect, aperture, focusDist float64) Camera {
   c := Camera{}
 
   c.origin = lookFrom
+  c.lensRadius = aperture / 2
+
+  theta := vFov * math.Pi / 180
+  halfHeight := math.Tan(theta / 2)
+  halfWidth := aspect * halfHeight
 
   w := lookFrom.Sub(lookAt).Normalize()
   u := vUp.Cross(w).Normalize()
   v := w.Cross(u)
 
-  c.lowerLeft = c.origin.Sub(u.MultiplyScalar(halfWidth)).Sub(v.MultiplyScalar(halfHeight)).Sub(w)
-  c.horizontal = u.MultiplyScalar(2 * halfWidth)
-  c.vertical = v.MultiplyScalar(2 * halfHeight)
+  x := u.MultiplyScalar(halfWidth * focusDist)
+  y := v.MultiplyScalar(halfHeight * focusDist)
+
+  c.lowerLeft = c.origin.Sub(x).Sub(y).Sub(w.MultiplyScalar(focusDist))
+  c.horizontal = x.MultiplyScalar(2)
+  c.vertical = y.MultiplyScalar(2)
+
+  c.w = w
+  c.u = u
+  c.v = v
 
   return c
 }
