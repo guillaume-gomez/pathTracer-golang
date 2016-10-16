c654b26e5d2f8ddcf8fa06ef018a34b618838f22
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-10-16 18:31:46 +0200

    WIP prepare to handle box hitRecord

--- primitives/hitable.go
+++ primitives/hitable.go
@@ -15,6 +15,16 @@ func buildHitRecordFromSphere(t float64, ray Ray, sphere *Sphere) HitRecord {
   }
 }
 
+//Work in progress
+func buildHitRecordFromBox(t float64, ray Ray, box Box) HitRecord {
+  return HitRecord{
+    T: t,
+    Point: Ray.Point(t),
+    Normal: Vector{},
+    Material: box.Material,
+  }
+}
+
 type Hitable interface {
   Hit(r Ray, tMin float64, tMax float64) (bool, HitRecord)
 }
